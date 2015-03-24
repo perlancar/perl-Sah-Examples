43f0a0d8f5f33b54c33cae746cb949c020b23196
perlancar (@pc-office) <perlancar@gmail.com>
2015-03-24 13:13:37 +0700

    Initial commit

---
+++ lib/Sah/Examples.pm
@@ -0,0 +1,22 @@
+package Sah::Examples;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Example Sah schemas
+
+=head1 DESCRIPTION
+
+This distribution contains various examples of L<Sah> schemas in its per-dist
+share dir.
+
+
+=head1 SEE ALSO
+
+L<Sah> specification, which contains a spectest in its per-dist share dir.
+
+L<Data::Sah>, Perl implementation for Sah.
+
+Various C<Sah::Schema::*> modules which contain schemas in Perl modules.
+
